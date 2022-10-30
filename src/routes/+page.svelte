8a67dc67f68d3f8b01ab32afe98afa28b853e5c2
Nic Bovee <nic@nicbovee.com>
2022-10-29 22:02:22 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import chili from '$lib/assets/chili.gif';
+</script>
+
+<div class="py-60 w-screen flex justify-center items-center bg-black">
+	<div class="text-white">
+		<h1 class="text-5xl font-bold font-sans lowercase">Bidet In The USA 🚽</h1>
+		<p>5 reasons you should clean your filthy asshole.</p>
+	</div>
+</div>
+<div class="py-80 w-screen flex justify-center items-center bg-black">
+	<div class="text-white">
+		<img src={chili} alt="The office chili cleanup" class="pb-8" />
+		<h2 class="text-5xl font-bold font-sans lowercase">#1 Toilet Paper Sucks</h2>
+		<p class="text-xl">Wiping with paper is like trying to wipe chili off the floor.</p>
+	</div>
+</div>
