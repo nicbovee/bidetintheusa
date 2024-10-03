a84ea5a53dcaa9f1ba61340786b6def5f0cd10df
Nic Bovee <nic@nicbovee.com>
2024-10-03 11:19:48 -0600

    add dakotah's reason

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,32 @@
 	import chili from '$lib/assets/chili.gif';
 </script>
 
-<div class="py-60 w-screen flex justify-center items-center bg-black">
-	<div class="text-white">
-		<h1 class="text-5xl font-bold font-sans lowercase">Bidet In The USA 🚽</h1>
-		<p>A few reasons you should clean your filthy asshole.</p>
+
+
+<div class="py-40 min-h-screen w-screen flex justify-center items-center bg-black">
+	<div>
+		<div class="text-white pb-24">
+			<h1 class="text-5xl font-bold font-sans lowercase">Bidet In The USA 🚽</h1>
+			<p>A few reasons you should clean your filthy asshole.</p>
+		</div>
+		<div class="text-white pb-12">
+			<img src={chili} alt="The office chili cleanup" class="pb-8" />
+			<h2 class="text-5xl font-bold font-sans lowercase">#1 Toilet Paper Sucks</h2>
+			<p class="text-xl">Wiping with paper is like trying to wipe chili off the floor.</p>
+		</div>
+		<div class="text-white max-w-screen-md pb-12">
+			<h2 class="text-5xl font-bold font-sans lowercase">#2 When you have the runs</h2>
+			<p class="text-xl">not wiping will save you from the agony of the chafe.</p>
+		</div>
+		<div class="text-white max-w-screen-md pb-12">
+			<h2 class="text-5xl font-bold font-sans lowercase">#3 No clogs</h2>
+			<p class="text-xl">With toilet paper in order to get a clean butt, you need to use kind of a lot. With a bidet, at the most you'll use one square to dry off.</p>
+		</div>
+		<div class="text-white">
+			<h2 class="text-5xl font-bold font-sans lowercase">There's more. Trust me.</h2>
+			<p class="text-xl">Your butt will thank me.</p>
+		</div>
 	</div>
+
 </div>
-<div class="py-80 w-screen flex justify-center items-center bg-black">
-	<div class="text-white">
-		<img src={chili} alt="The office chili cleanup" class="pb-8" />
-		<h2 class="text-5xl font-bold font-sans lowercase">#1 Toilet Paper Sucks</h2>
-		<p class="text-xl">Wiping with paper is like trying to wipe chili off the floor.</p>
-	</div>
-</div>
-<div class="py-80 w-screen flex justify-center items-center bg-black">
-	<div class="text-white">
-		<h2 class="text-5xl font-bold font-sans lowercase">There's more. Trust me.</h2>
-		<p class="text-xl">Your butt will thank me.</p>
-	</div>
-</div>
+
